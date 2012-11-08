ce371d428749545bb0edad7a2ca0df7a5827ef62
Jan Oelze <janoelzelebt@googlemail.com>
2012-11-08 13:08:02 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-redmyne
+Redmyne
 =======
 
-command-line redmyne issue overview 
+Redmyne gives you a quick glimpse at your open issues.
+
+## Usage
+
+```
+$ python /path/redmyne/app.py -k <YOURAPIKEY>
+| You've got 2 open isssues:
+|
+| ISSUE #981 - Prio: ALARM
+|   >> Check permissions on client account @ SecretProject
+|   Due: 2012/11/08
+|   Estimated Hours: 1.0
+| ISSUE #980 - Prio: Normal
+|   >> Fix Drag'n'Drop Bug @ BugProject
+|   Due: 2012/11/09
+|   Estimated Hours: 4.0
+```
---
+++ app.py
@@ -0,0 +1,47 @@
+import urllib
+import json
+import urllib2
+from optparse import OptionParser
+
+
+BASE_URL = 'http://cos.gfedev.de/'
+API_KEY = ''
+M1 = '\033[1;35m'
+M2 = '\033[1;m'
+
+
+def get_opts():
+    parser = OptionParser()
+    parser.add_option("-k", "--key", dest="key", help="Your Redmine API Key.", metavar="KEY")
+    return parser.parse_args()
+
+
+def get_issues():
+    params = {
+        'assigned_to_id': 'me',
+        'key': API_KEY,
+        'set_filter': '1',
+        'sort': 'priority%3Adesc%2Cupdated_on%3Adesc'
+    }
+    path = 'issues.json?%s' % urllib.urlencode(params)
+    r = urllib2.urlopen('%s%s' % (BASE_URL, path))
+    return r.read()
+
+def l(m): print '| %s' % m
+
+
+def format_issues(json_string):
+    pass
+    response = json.loads(json_string)
+    l("You've got %s open isssues:\n|" % (response['total_count']))
+    for issue in response['issues']:
+        if issue['subject'] != '':
+            l('ISSUE #%s - Prio: %s' % (issue['id'], issue['priority']['name'])) 
+            l('\t>> %s @ %s' % (issue['subject'], issue['project']['name'])) 
+            l('\tDue: %s' % issue['due_date']) 
+            l('\tEstimated Hours: %s' % (issue['estimated_hours'])) 
+
+if __name__ == "__main__":
+    (opts, args) = get_opts()
+    API_KEY = opts.key
+    format_issues(get_issues())
