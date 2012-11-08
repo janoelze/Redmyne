6f9acba0915997de4aefb71ce0fddc6293731d0a
Jan Oelze <janoelzelebt@googlemail.com>
2012-11-08 19:22:59 +0100

    fixed problem with no-existent dict-keys

--- app.py
+++ app.py
@@ -8,15 +8,20 @@ BASE_URL = 'http://cos.gfedev.de/'
 API_KEY = ''
 
 
+def l(m): print '| %s' % m
+
+
 def get_opts():
     parser = OptionParser()
-    parser.add_option("-k", "--key", dest="key", help="Your Redmine API Key.", metavar="KEY")
+    parser.add_option("-k", "--key", dest="key",
+                      help="Your Redmine API Key.", metavar="KEY")
     return parser.parse_args()
+
 
 def get_percentage(ratio):
     i = (int(ratio))
     max = 10
-    return '[{0}{1}] {2}%'.format('#'*(i/10),' '*(max-i/10),i)
+    return '[%s%s] %s%%' % ('#' * (i / 10), ' ' * (max - i / 10), i)
 
 
 def get_issues():
@@ -31,21 +36,34 @@ def get_issues():
     return r.read()
 
 
-def l(m): print '| %s' % m
+def print_issue(issue):
+    l('ISSUE #%s' % (issue['id']))
+    l('\t>> %s @ %s' % (issue['subject'], issue['project_name']))
+    l('\tDue: %s' % issue['due_date'])
+    l('\tEstimated Hours: %s' % (issue['estimated_hours']))
+    l('\tProgress: %s' % (get_percentage(issue['done_ratio'])))
+    l('')
 
 
 def format_issues(json_string):
+    print json_string
     response = json.loads(json_string)
     l("You've got %s open issues:\n|" % (response['total_count']))
     for issue in response['issues']:
         if issue['subject'] != '':
-            l('ISSUE #%s - Prio: %s' % (issue['id'], issue[
-              'priority']['name']))
-            l('\t>> %s @ %s' % (issue['subject'], issue['project']['name']))
-            l('\tDue: %s' % issue['due_date'])
-            l('\tEstimated Hours: %s' % (issue['estimated_hours']))
-            l('\tProgress: %s' % (get_percentage(issue['done_ratio'])))
-            l('')
+            
+            default = '-'
+            
+            issue_f = {
+                'id': issue.get('id', default),
+                'subject': issue.get('subject', default),
+                'project_name': issue['project'].get('name', default),
+                'due_date': issue.get('due_date', default),
+                'estimated_hours': issue.get('estimated_hours', default),
+                'done_ratio': issue.get('done_ratio', default)
+            }
+            print_issue(issue_f)
+
 
 if __name__ == "__main__":
     (opts, args) = get_opts()
